94769dcbcc0e0a34631a75fca0ba39ee9ea14fd3
MAC <mac@MACs-MacBook-Pro.local>
2026-01-29 09:52:15 +0530

    complete user

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -6,6 +6,8 @@ import { SanityLive } from "@/sanity/lib/live";
 import { CartSheet } from "@/components/CartSheet";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Toaster } from "sonner";
+import { ChatSheet } from "@/components/ChatSheet";
+import { AppShell } from "@/components/AppShell";
 
 function Layout({ children }: { children: React.ReactNode }) {
   return (
@@ -13,9 +15,12 @@ function Layout({ children }: { children: React.ReactNode }) {
       <SanityProvider>
         <CartStoreProvider>
           <ChatStoreProvider>
-            <Header />
-            <main>{children}</main>
+            <AppShell>
+              <Header />
+              <main>{children}</main>
+            </AppShell>
             <CartSheet />
+            <ChatSheet />
             <Toaster position="bottom-center" />
             <SanityLive />
           </ChatStoreProvider>
